fcba9548db5946f139c4149679ee646177b4d25b
Regan <regan@dotsure.co.za>
2019-03-01 09:57:46 +0200

    More Stuff

--- main.js
+++ main.js
@@ -13,12 +13,15 @@ const app = new Vue({
     data: {
         name: 'John Doe',
         location: 'New York',
+        profile_pic: './images/id-image.jpeg',
+        about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis eros quis risus ornare volutpat et quis sapien. Mauris id erat convallis, ullamcorper elit et, tristique lacus',
         stats: {
             alerts: 3,
             places: 4,
             shots: 10,
             friends: 30
         },
+        settings: false,
         friend_list: {
           0 : {
             pr_pic: './images/wade.jpeg',
@@ -35,7 +38,7 @@ const app = new Vue({
           2 : {
             pr_pic: './images/stefan.jpeg',
             full_name: 'Stefan Du Plessis',
-            active: true,
+            active: false,
             last_seen_unix: 1551269100
           },
           3 : {
@@ -43,7 +46,8 @@ const app = new Vue({
             full_name: 'Ruth Duthie',
             active: false,
             last_seen_unix: 1551269100
-          }
+          },
+          
         }
     },
     computed: {
